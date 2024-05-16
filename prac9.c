0b6fc1d45c2f007cfea271c5033e1e485fb5f22d
Gayatri Deshkar <110033600+gayatrideshkar@users.noreply.github.com>
2024-05-16 09:42:17 +0530

    Update prac9.c

--- prac9.c
+++ prac9.c
@@ -1,74 +1,26 @@
-#include <stdio.h> 
-#include <stdbool.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#define MAX_CODE_LINES 100 
-#define MAX_CODE_LENGTH 50 
-// Structure to represent a three-address code line 
-typedef struct { 
- char op; // OperaƟon: '+', '-', '*', '/' 
- int result; // Result variable index 
- int arg1; // Argument 1 variable index or constant value 
- int arg2; // Argument 2 variable index or constant value 
-} ThreeAddressCode; 
-ThreeAddressCode code[MAX_CODE_LINES]; // Array to store three-address code lines 
-int codeCount = 0; // Current count of code lines 
-// FuncƟon to add a three-address code line to the array 
-void addCodeLine(char op, int result, int arg1, int arg2) { 
- code[codeCount].op = op; 
- code[codeCount].result = result; 
- code[codeCount].arg1 = arg1; 
- code[codeCount].arg2 = arg2; 
- codeCount++; 
-} 
-// FuncƟon to perform basic opƟmizaƟons on three-address code 
-void opƟmizeCode() {
- for (int i = 0; i < codeCount; i++) { 
- // Check if both arguments are constants 
- if (code[i].arg1 >= 0 && code[i].arg2 >= 0) { 
- // Perform constant folding 
- int value; 
- switch (code[i].op) { 
- case '+': 
- value = code[i].arg1 + code[i].arg2; 
- break; 
- case '-': 
- value = code[i].arg1 - code[i].arg2; 
- break; 
- case '*': 
- value = code[i].arg1 * code[i].arg2; 
- break; 
- case '/': 
- value = code[i].arg1 / code[i].arg2; 
- break; 
- } 
- // Replace the current line with a simplified assignment if applicable 
- code[i].op = '='; 
- code[i].arg1 = value; 
- code[i].arg2 = -1; // No need for a second argument in an assignment
- }
- }
-}
-// FuncƟon to print the opƟmized three-address code
-void printOpƟmizedCode() {
- for (int i = 0; i < codeCount; i++) {
- if (code[i].op == '=') {
- prinƞ("t%d = %d\n", code[i].result, code[i].arg1);
- } else {
- prinƞ("t%d = t%d %c t%d\n", code[i].result, code[i].arg1, code[i].op, code[i].arg2);
- }
- }
-}
-int main() {
- // Example three-address code
- addCodeLine('+', 1, 2, 3);
- addCodeLine('*', 4, 1, 2);
- addCodeLine('-', 5, 4, 3);
- addCodeLine('*', 6, 5, 6);
- prinƞ("Original three-address code:\n");
- printOpƟmizedCode();
- prinƞ("\nOpƟmized three-address code:\n");
- opƟmizeCode();
- printOpƟmizedCode();
- return 0;
-}
+i = 0
+j = 0
+min_idx
+if i>n-1 goto (23)
+if j>=n goto (13)
+t1 = arr[j]
+t2 = arr[min_idx]
+if t1<t2 , goto(10)
+goto(13)
+min_idx j;
++3 = j+1;
+j = +3
+goto
+swap t2 with arr[1]
+t4 = arr[i]
+t5 = t2
+t2 = t4
+t4 = t5
+t6 = 1+1
+i = t6
+j = 1
+goto[]
+printf(sorted array)
+for i = 0 to n-1 goto(25)
+printf("%d",t4)
+goto(24)
